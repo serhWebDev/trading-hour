e795311eafaaeb6706f1394946a307bc305c104d
panytsch <romeo.crary@gmail.com>
2019-02-08 20:41:54 +0200

    second part of refactor

--- src/components/ListOfInstruments.js
+++ src/components/ListOfInstruments.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import TradingHoursData from "./TradingHoursData";
 import StatusTradingHours from "./StatusTradingHours";
--- src/components/TimeLimit.js
+++ src/components/TimeLimit.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 const convertWithZero = v => (v < 10 ? "0" + v : v);
 
